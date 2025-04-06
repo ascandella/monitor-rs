970d98d7fabe1307c5538de46ba5790c8dc83d5b
Aiden Scandella <git@sca.ndella.com>
2025-04-06 13:54:30 -0700

    add some TODOs

--- src/manager.rs
+++ src/manager.rs
@@ -10,11 +10,8 @@ pub struct Manager {
 async fn handle_btle_events(
     adapter: &btleplug::platform::Adapter,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    println!("Starting BTLE event loop");
     let mut events = adapter.events().await?;
-    // start scanning for devices
-    // TODO
-    //
+
     while let Some(event) = events.next().await {
         match event {
             CentralEvent::DeviceDiscovered(id) => {
@@ -25,9 +22,6 @@ async fn handle_btle_events(
                     .map(|local_name| format!("Name: {local_name}"))
                     .unwrap_or_default();
                 println!("DeviceDiscovered: {:?} {}", id, name);
-            }
-            CentralEvent::StateUpdate(state) => {
-                println!("AdapterStatusUpdate {:?}", state);
             }
             CentralEvent::DeviceDisconnected(id) => {
                 println!("DeviceDisconnected: {:?}", id);
@@ -54,20 +48,21 @@ impl Manager {
     pub async fn run_loop(mut self) -> Result<(), Box<dyn std::error::Error>> {
         self.adapter.start_scan(ScanFilter::default()).await?;
 
-        // TODO
+        // Handle incoming MQTT messages (e.g. arrival scan requests)
         tokio::task::spawn(async move {
+            // TODO: Need to pass ability to trigger a BTLE scan to the event loop
             crate::mqtt::MqttClient::event_loop(&mut self.mqtt_event_loop).await;
         });
 
         // Run on a separate thread as these currently block
         let btle_handle = std::thread::spawn(move || {
+            // TODO: Need to pass the ability to publish MQTT messages to this function
             if let Err(err) = block_on(handle_btle_events(&self.adapter)) {
                 eprintln!("Error handling BTLE events: {:?}", err);
             }
             println!("Done handling BTLE events")
         });
 
-        // TODO handle error
         if let Err(err) = btle_handle.join() {
             eprintln!("Error handling btle events: {:?}", err);
         }
